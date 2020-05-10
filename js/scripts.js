e64b24ec7bb85f58b98743a60d74b20612d66e4e
Aalok766626 <55963268+Aalok766626@users.noreply.github.com>
2020-05-10 11:45:37 -0400

    automate login

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,5 @@
 $(document).ready(function(){
-            $("#mycarousel").carousel( { interval: 2000 } );
-            $("#carousel-pause").click(function(){
-                $("#mycarousel").carousel('pause');
-            });
-            $("#carousel-play").click(function(){
-                $("#mycarousel").carousel('cycle');
-            });
-        });
-$(document).ready(function(){
-            $("#reserveBtn").click(function(){
-                $("#myModal").modal('show');
-            });
-        });
-
-$(document).ready(function(){
-        $("#loginBtn").click(function(){
-            $("#login").modal('show');
-                });
-            });
+        setTimeout(function(){
+           $("#login").modal('show');
+        },1500);
+    });
