f4e7f80c27321c9cd41ba369ef25cbd7fbc6700b
Aalok766626 <55963268+Aalok766626@users.noreply.github.com>
2020-05-11 00:57:23 +0530

    Second_page linked

--- js/functionality.js
+++ js/functionality.js
@@ -4,7 +4,8 @@ function validatte(){
     var rem=document.getElementById("val").value;
     var year=document.getElementById("year").value;
     var details="Name: "+str1+"\n"+"Roll No.: "+str2+"\n"+"Year: "+year+"\n"+"Remember me: "+rem+"\n";
-    window.confirm("Please confirm your details."+"\n"+details+"\n"+"Press OK to continue...\n");
+    //window.confirm("Please confirm your details."+"\n"+details+"\n"+"Press OK to continue...\n");
+    window.open('Second_Sem.html','_self');
 }
 function valuech(){
     document.getElementById("val").value="on"
