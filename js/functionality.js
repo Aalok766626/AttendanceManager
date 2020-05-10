f63bef7f47f0c23ad0665441dc7054e95b96ace8
Aalok766626 <55963268+Aalok766626@users.noreply.github.com>
2020-05-10 19:17:29 -0400

    Update functionality.js

--- js/functionality.js
+++ js/functionality.js
@@ -4,28 +4,59 @@ function validatte(){
     var remember=document.getElementById("val").value;
     var year=document.getElementById("year").value;
     var details="Roll No.: "+roll+"\n"+"Password: "+pasword+"\n"+"Year: "+year+"\n"+"Remember me: "+remember+"\n";
-    
-    //window.confirm("Everything right");
-   if (roll==""||roll==" ")
-    { 
-        alert("Invalid Roll");
-    }
-    if (pasword==""||pasword==" ")
+    if(rollvalidator()&&passvalidator())
     {
-        alert("Invalid Password")
+        window.confirm("Please confirm your details."+"\n\n"+details+"\n"+"Press OK to continue...\n");
+        window.location="Second_Sem.html";
     }
-    else
-    { window.confirm("Please confirm your details."+"\n\n"+details+"\n"+"Press OK to continue...\n");}
+    else{
+        if(!rollvalidator())
+        {
+            alert("Please enter a valid Roll No.\n");
+        }
+        else if(!passvalidator())
+        {
+            alert("Invalid Password !!!\n1.Password must be minimum 6 character\n2.Password must have atleast one Uppercase\n    and a Lowercase letter\n3.Password must have atleast one digit\n");
+        }
+        return false;
+    }
        
-
-
 }
 function valuech(){
     document.getElementById("val").value="on"
 }
 
-function rollpassvalidator(){
+function rollvalidator(){
     var roll=document.getElementById("inputemail").value;
+    var rollformat=/([1-2])([0-9])\d{4}$/;
+
+    if (rollformat.test(roll)&&roll.length==6)
+    { 
+       return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+function passvalidator()
+{
     var pasword=document.getElementById("inputpass").value;
-    
+    var paswordformat=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+    if (paswordformat.test(pasword))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
